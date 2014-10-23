9d8f432414964200ec52f248c56edfb16bf72b09
x00107805 <x00107805@gmail.com>
2014-10-23 21:44:27 +0100

    shell script upload

---
+++ x00107805-grades.sh
@@ -0,0 +1,11 @@
+echo "test case 1 for combined less than 50"						
+java grades 50 50
+echo "test case 2 for exam scores less than 40"						
+java grades 40 40
+java grades 39 60
+echo "test case 3 for practical scores less than 40"						
+java grades 20 40 
+java grades 100 39 
+echo "For all other inputs"					
+java x00107805-grades a n
+
